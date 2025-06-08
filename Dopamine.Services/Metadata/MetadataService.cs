b5c8449a94f4c3079d878e03dee92b2c1021a5ca
Jesus Hernandez Tlanepantla <aztaroth666@gmail.com>
2025-06-07 19:35:46 -0600

    ATL Project is added.

--- Dopamine.Services/Metadata/MetadataService.cs
+++ Dopamine.Services/Metadata/MetadataService.cs
@@ -84,13 +84,14 @@ namespace Dopamine.Services.Metadata
                     await this.updater.UpdateFileMetadataAsync(new FileMetadata[] { fmd }.ToList());
                 }
                 //// Only for FLAC's
-                //if (Path.GetExtension(path).ToLower().Equals(FileFormats.FLAC))
-                //{
-                //    // Initialize with a file path
-                //    var theTrack = new ATL.Track(path);
-                //    theTrack.Popularity = rating;
-                //    theTrack.Save();
-                //}
+                if (Path.GetExtension(path).ToLower().Equals(FileFormats.FLAC))
+                {
+                    // Initialize with a file path
+                    var theTrack = new ATL.Track(path);
+                    theTrack.Popularity = rating;
+                    theTrack.Year = 1987;
+                    theTrack.Save();
+                }
             }
 
             this.RatingChanged(new RatingChangedEventArgs(path.ToSafePath(), rating));
